96bd237a0332c8500a75b9ae300f336762cb04ff
Mohamed CHERIF <mocherifh0@gmail.com>
2024-12-14 13:44:37 +0100

    Update singleLinkedList.c

--- singleLinkedList.c
+++ singleLinkedList.c
@@ -1,89 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node 
-{
-    int data ;
-    struct node *next ;
-}
-node ;
-node *list;
+typedef struct node {
+    int data;
+    struct node *next;
+} node;
 
-node * create (int val)
-{
+
+node *create(int val) {
     node *n = malloc(sizeof(node));
-    n->data = val ;
-    n->next = NULL ;
-
-    return n ;
-}
-
-node *insert(node *head , int val ) 
-{
-    node *new = malloc(sizeof(node));
-    new->data = val ;
-    new->next = head ;
-    head = new ;
-
-    return head ;
-
-}
-int count = 0;
-// recursion output
-int output (node *lists)
-{
-   
-    if (lists != NULL) {
-        printf("%d\n" , lists->data);
-        count = count + 1 ;
-        output(lists->next);
+    if (n == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
     }
-       
-    else {
-         printf("\ncount : %d\n", count);
-    }
-   
-    
+    n->data = val;
+    n->next = NULL;
+    return n;
 }
 
-int searching (node *lists , int target)
-{
-    node *tmp = lists ;
-    for (node *tmp = lists ; tmp!= NULL ; tmp = tmp->next)
-    {
-        if (tmp->data == target)
-        {
-            printf("found ..");
-            return 0 ;
+
+node *insert(node *head, int val) {
+    node *newNode = malloc(sizeof(node));
+    if (newNode == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = val;
+    newNode->next = head;
+    return newNode;
+}
+
+void output(node *list) {
+    static int count = 0; // Static ensures count persists across recursive calls
+    if (list != NULL) {
+        printf("%d\n", list->data);
+        count++;
+        output(list->next);
+    } else {
+        printf("\nCount: %d\n", count);
+    }
+}
+
+int searching(node *list, int target) {
+    for (node *tmp = list; tmp != NULL; tmp = tmp->next) {
+        if (tmp->data == target) {
+            printf("Found %d\n", target);
+            return 1;
         }
     }
-    
-    printf("not found...") ;
-    return 1 ;
+    printf("Not found: %d\n", target);
+    return 0;
 }
 
-void destroy (node *l)
-{
-    node *tmp ;
-    while (l != NULL) 
-    {
-        tmp = l->next ;
-        free(l);
-        l = tmp ;
+
+void destroy(node *list) {
+    node *tmp;
+    while (list != NULL) {
+        tmp = list->next;
+        free(list);
+        list = tmp;
     }
 }
 
-int main (void)
-{   
-    list = NULL ;
-    list = create (6) ;
-    list = insert(list, 8) ;
-    list = insert(list, 4) ;
-    list = insert(list, 2) ;
-    
+int main(void) {
+    node *list = NULL;
+    list = create(6);
+    list = insert(list, 8);
+    list = insert(list, 4);
+    list = insert(list, 2);
 
-   output(list);
-   searching (list, 10);
-   destroy (list);
+    output(list);
+    searching(list, 10);
+    destroy(list);
+
+    return 0;
 }
-
